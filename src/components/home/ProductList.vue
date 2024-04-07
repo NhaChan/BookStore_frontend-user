ad27f86d71b41ce151c91ec9477a5ecd9be0100b
NhaChan <chanb2014731@student.ctu.edu.vn>
2024-04-07 22:02:45 +0700

    Hien thi san pham

---
+++ src/components/home/ProductList.vue
@@ -0,0 +1,45 @@
+<script>
+// import CartService from "@/services/cart.service";
+import Cookies from 'js-cookie';
+export default {
+    props: {
+        products: { type: Array, default: [] },
+        activeIndex: { type: Number, default: -1 },
+    },
+    emits: ["update:activeIndex"],
+    methods: {
+        updateActiveIndex(index) {
+            this.$emit("update:activeIndex", index);
+        },
+    }
+};
+</script>
+
+<template>
+    <div class="row mb-4">
+        <div class="col-lg-2 col-md-6 col-sm-12 mb-4" v-for="(product, index) in products" :key="product._id">
+            <div class="card shadow-sm">
+                <img :src="product.imageUrl" class="card-img-top mt-3 align-self-center d-flex " alt="Hình ảnh"
+                    style="height: 200px; width: 200px; object-fit: contain;">
+                <div class="card-body">
+                    <h5 class="card-title">{{ product.title }}</h5>
+                    <p class="card-text">Tác giả: {{ product.author }}</p>
+                    <p class="card-text">Thể loại: {{ product.genre }}</p>
+                    <p class="card-text">Số lượng còn lại: {{ product.quantity }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+.card {
+    margin-top: 15px;
+}
+
+.card:hover {
+    transform: translateY(-5px);
+}
+.card:hover {
+    background-color: #f8f9fa;
+}
+</style>
---
+++ src/views/home/HomePage.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="mt-3 col-lg-12 col-md-6 col-sm-2">
+                <ProductList v-if="filteredProductsCount > 0" :products="filteredProducts"
+                    v-model:activeIndex="activeIndex" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import ProductList from "@/components/home/ProductList.vue";
+import ProductService from "@/services/book.service";
+export default {
+    components: {
+        ProductList,
+    },
+    data() {
+        return {
+            products: [],
+            activeIndex: -1,
+            selectedGenres: [],
+            genres: [],
+        };
+    },
+    computed: {
+        filteredProducts() {
+            if (!this.selectedGenres.length) return this.products;
+            return this.products.filter(contact => this.selectedGenres.includes(contact.genre));
+        },
+        filteredProductsCount() {
+            return this.filteredProducts.length;
+        },
+    },
+    methods: {
+        async retrieveProducts() {
+            try {
+                this.products = await ProductService.getAll();
+                this.genres = this.extractGenres(this.products);
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        extractGenres(products) {
+            const genresSet = new Set();
+            products.forEach(contact => {
+                genresSet.add(contact.genre);
+            });
+            return Array.from(genresSet);
+        },
+        refreshList() {
+            this.retrieveProducts();
+            this.activeIndex = -1;
+        },
+        toggleGenre(genre) {
+            if (this.selectedGenres.includes(genre)) {
+                this.selectedGenres = this.selectedGenres.filter(item => item !== genre);
+            } else {
+                this.selectedGenres.push(genre);
+            }
+        }
+    },
+    mounted() {
+        this.refreshList();
+    },
+};
+</script>
+
+<style>
+.category-title {
+    font-size: 1.2rem;
+    margin-bottom: 1rem;
+}
+
+.category-item {
+    cursor: pointer;
+    padding: 0.5rem 1rem;
+    transition: background-color 0.3s;
+}
+
+.category-item:hover {
+    background-color: #f0f0f0;
+}
+</style>
