234ae7d556c562acc064dfe1a3521983710daf0c
NhaChan <chanb2014731@student.ctu.edu.vn>
2024-04-07 22:10:33 +0700

    productlist

--- src/components/home/ProductList.vue
+++ src/components/home/ProductList.vue
@@ -17,15 +17,14 @@ export default {
 
 <template>
     <div class="row mb-4">
-        <div class="col-lg-2 col-md-6 col-sm-12 mb-4" v-for="(product, index) in products" :key="product._id">
+        <div class="col-lg-2 col-md-4 col-sm-6 col-12 mb-4" v-for="(product, index) in products" :key="product._id">
             <div class="card shadow-sm">
-                <img :src="product.imageUrl" class="card-img-top mt-3 align-self-center d-flex " alt="Hình ảnh"
+                <img :src="product.imageUrl" class="card-img-top mt-3 align-self-center d-flex" alt="Hình ảnh"
                     style="height: 200px; width: 200px; object-fit: contain;">
                 <div class="card-body">
-                    <h5 class="card-title">{{ product.title }}</h5>
+                    <p class="card-title text-success fw-bold">{{ product.title }}</p>
                     <p class="card-text">Tác giả: {{ product.author }}</p>
                     <p class="card-text">Thể loại: {{ product.genre }}</p>
-                    <p class="card-text">Số lượng còn lại: {{ product.quantity }}</p>
                 </div>
             </div>
         </div>
@@ -39,6 +38,7 @@ export default {
 .card:hover {
     transform: translateY(-5px);
 }
+
 .card:hover {
     background-color: #f8f9fa;
 }
