b03468df22f2233335a2e46c1ddb765df7bc4100
nhachan <chanb2014731@student.ctu.edu.vn>
2024-04-25 22:59:30 +0700

    mdified cart

--- src/components/cart/CartList.vue
+++ src/components/cart/CartList.vue
@@ -12,34 +12,23 @@
                                     </a>
                                 </h4>
                             </div>
-                            <!--sản phẩm -->
+                            <!-- Sản phẩm -->
                             <div class="col-lg-6">
-                                <div>
-                                    <div v-for="(product, index) in products" :key="product._id"
-                                        :class="{ active: index === activeIndex }" class="card mb-3 border-0 bg-light" 
-                                        style="transform: translateY(0px);">
-                                        <div class="card-body">
-                                            <div class="d-flex justify-content-between">
-                                                <div class="d-flex flex-row align-items-center">
-                                                    <div><router-link :to="'/product/' + product._id">
-                                                        <img :src="product.imageUrl"
-                                                            class="img-fluid rounded-3 d-none d-sm-block"
-                                                            alt="Shopping item" style="width: 65px;" />
-                                                        </router-link>
-                                                    </div>
-                                                    <div class="ms-3">
-                                                        <p class="fs-5 text-success" style=" text-decoration: none;">
-                                                                {{ product.title }}
-                                                        </p>
-                                                        <p class="small mb-0">Tác giả: {{ product.author }}</p>
-                                                    </div>
-                                                </div>
+                                <div class="row">
+                                    <div class="col-lg-4 col-md-4 col-sm-6 col-12 mb-4"
+                                        v-for="(product, index) in products" :key="product._id"
+                                        :class="{ active: index === activeIndex }">
+                                        <div class="card bg-light d-flex align-items-center" style="transform: translateY(0px);">
+                                            <router-link :to="'/product/' + product._id">
+                                                <img :src="product.imageUrl"
+                                                    class="card-img-top mt-3 align-self-center d-flex mx-auto"
+                                                    alt="Shopping item" style="width: 80%;" />
+                                            </router-link>
+                                            <div class="card-body">
+                                                <p class="card-title text-success" style="height: 50px;" >{{ product.title }}</p>
                                                 <div class="d-flex flex-row align-items-center justify-content-between">
-                                                    <h6 class="fw-normal mb-0 me-3">Số lượng:</h6>
-                                                    <div class="row" style="width: 100px;">
-                                                        <h5 class="fw-normal mb-0">{{ product.quantity }}</h5>
-                                                    </div>
-                                                    <button class="btn ml-3 btn-danger"
+                                                    <div class="card-text">Số lượng: {{ product.quantity }}</div>
+                                                    <button class="btn ml-3 text-danger"
                                                         @click="deleteProduct(product._id)">
                                                         <i class="fas fa-trash-alt"></i>
                                                     </button>
@@ -49,49 +38,39 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-lg-5">
-                                <div class="card bg-light rounded-3 border-0" style="transform: translateY(0px);">
-                                    <div class="card-body">
-                                        <div class="d-flex justify-content-between align-items-center mb-4">
-                                            <h5 class="mb-0">Thông tin</h5>
-                                        </div>
-                                        <form class="mt-4" @submit.prevent="submitForm">
-                                            <div>
-                                                <div class="form-group mb-4 d-none ">
-                                                    <label for="userId" class="form-label">User ID</label>
-                                                    <input type="text" id="userId" class="form-control form-control-lg"
-                                                        v-model="userId">
-                                                </div>
-                                                <div class="form-group mb-4">
-                                                    <label for="name" class="form-label">Tên người mượn</label>
-                                                    <input type="text" id="name" class="form-control form-control-lg"
-                                                        v-model="name">
-                                                </div>
-                                                <!-- <div class="form-group mb-4">
-                                                    <label for="name" class="form-label">Email</label>
-                                                    <input type="text" id="name" class="form-control form-control-lg"
-                                                        v-model="name">
-                                                </div> -->
-                                                <div class="form-group mb-4">
-                                                    <label for="ngayMuon" class="form-label">Ngày mượn</label>
-                                                    <input type="date" id="ngayMuon"
-                                                        class="form-control form-control-lg" v-model="ngayMuon"
-                                                        required>
-                                                </div>
-                                                <div class="form-group mb-4 text-primary fw-bold"> Phải trả sách trước thời hạn 2 tuần kể từ ngày mượn!</div>
-                                                <!-- <div class="form-group mb-4">
-                                                    <label for="ngayTra" class="form-label">Ngày trả</label>
-                                                    <input type="date" id="ngayTra" class="form-control form-control-lg"
-                                                        v-model="ngayTra" required>
-                                                </div> -->
-                                                
-                                                <button type="submit" class="btn btn-success btn-lg bg-success">
-                                                    Mượn
-                                                </button>
+                            <div class="col-md-5 col-lg-4 bg-light mt-3 ms-3">
+                                <h4 class="d-flex justify-content-between align-items-center mb-3">
+                                    <span class="text-primary">Thông tin</span>
+                                </h4>
+                                <form class="mt-4" @submit.prevent="submitForm">
+                                    <ul class="form-group mb-3 list-group">
+                                        <li class="list-group-item d-flex justify-content-between lh-sm">
+                                            <div class="form-group mb-4">
+                                                <label for="name" class="form-label">Người mượn</label>
+                                                <input type="text" id="name" class="form-control form-control-sm"
+                                                    v-model="name">
                                             </div>
-                                        </form>
+                                        </li>
+                                        <li class="list-group-item d-flex justify-content-between lh-sm">
+                                            <div class="form-group mb-4">
+                                                <label for="ngayMuon" class="form-label">Ngày mượn</label>
+                                                <input type="date" id="ngayMuon" class="form-control form-control-sm"
+                                                    v-model="ngayMuon" required>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item bg-body-tertiary">
+                                            <div class="text-success">
+                                                <h6 class="my-0"> Phải trả sách trước hạn 2 tuần kể từ ngày mượn!
+                                                </h6>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                    <div class="input-group card">
+                                        <button type="submit" class="btn btn-success btn-group-sm bg-success">
+                                            Mượn
+                                        </button>
                                     </div>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
@@ -100,6 +79,8 @@
         </div>
     </section>
 </template>
+
+
 
 
 <script>
@@ -179,4 +160,3 @@ export default {
     }
 };
 </script>
-
